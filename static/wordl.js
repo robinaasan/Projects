a5435722d1f50acead7b0412112eb99af5e5b477
robinaasan <robin.aasan@hotmail.com>
2022-04-02 14:13:32 +0200

    ok

---
+++ static/wordl.js
@@ -0,0 +1,17 @@
+const wordlRad = {
+    props: ["ord"],
+    template: /*html*/`
+        <label for="box">
+        <input type="text" class="box" id="" name="box" size="1" maxlength=1>
+        <input type="text" class="box" id="" name="box" size="1" maxlength=1>
+        <input type="text" class="box" id="" name="box" size="1" maxlength=1>
+        <input type="text" class="box" id="" name="box" size="1" maxlength=1>
+        <input type="text" class="box" id="" name="box" size="1" maxlength=1>
+        </label>
+    `,
+    data: function()    {
+        return  {
+            
+        }
+    }
+}
